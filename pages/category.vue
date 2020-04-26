772c0025691c7dc3cba4fc279cddc99aca589566
Hiroaki Yamada <54619750+vivaruckk17@users.noreply.github.com>
2020-04-27 01:56:49 +0900

    Update category.vue

--- pages/category.vue
+++ pages/category.vue
@@ -2,7 +2,7 @@
   <members-only>
     <div id="app">
       <h1>ログインしていないと見れないページ</h1>
-      <a href="/account">アカウントページへ</a>
+      <a href="/accunt">アカウントページへ</a>
     </div>
   </members-only>
 </template>
