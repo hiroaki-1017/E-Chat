e4d514adc2e2f5737afbeb1e560a628aae4de93a
REiJi <shino327gh@gmail.com>
2020-04-27 01:24:09 +0900

    SDKの追加

--- plugins/firebase.js
+++ plugins/firebase.js
@@ -2,7 +2,13 @@ import firebase from 'firebase'
 
 if (!firebase.apps.length) {
   firebase.initializeApp({
-    
+    apiKey: "AIzaSyAKRejR__XXB3hufe5ghtMwzrKyU5Q4NDM",
+    authDomain: "worktalk-10bef.firebaseapp.com",
+    databaseURL: "https://worktalk-10bef.firebaseio.com",
+    projectId: "worktalk-10bef",
+    storageBucket: "worktalk-10bef.appspot.com",
+    messagingSenderId: "837310087476",
+    appId: "1:837310087476:web:e0021e6bd83cbb32026561"
 
 
     
