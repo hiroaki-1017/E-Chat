31127f411df7a3681c4788dc5e24ab9b30098eee
Hiroaki Yamada <54619750+vivaruckk17@users.noreply.github.com>
2020-04-28 04:06:37 +0900

    login.vueフロントほぼ完成

--- pages/category.vue
+++ pages/category.vue
@@ -1,8 +1,34 @@
 <template>
   <members-only>
     <div id="app">
-      <h1>ログインしていないと見れないページ</h1>
       <a href="/accunt">アカウントページへ</a>
+      <div class="mainCategory" v-for="(item, key) in categoryItem" :key="key">
+        <div class="category">
+          {{item.name}}
+        </div>
+      </div>
     </div>
   </members-only>
 </template>
+<script>
+export default {
+  el: '#app',
+  data: {
+    categoryItem: [
+      {name: '公務員'}
+    ]
+  }
+}
+</script>
+<style>
+@media screen and (max-width: 720px) {
+
+  .category{
+    height: 110px;
+    width: 180px;
+    background-color: #00CDFF;
+  }
+
+}
+
+</style>
--- pages/login.vue
+++ pages/login.vue
@@ -2,85 +2,155 @@
   <div>
     <!-- ログイン中に表示される画面 -->
     <div v-if="isAuthenticated">
-      {{ user.email }}でログイン中です<br>
-      <button v-on:click="logout">ログアウト</button><br>
+      {{ user.email }}でログイン中です
+      <br />
+      <button v-on:click="logout">ログアウト</button>
+      <br />
       <a href="/category">カテゴリーページへ</a>
     </div>
     <!-- ログインしていない時に表示される画面 -->
-    <div v-else>
-      <h1>ワークトークにようこそ</h1>
-      メール<br>
-      <input type="text" v-model="email"><br>
-      パスワード<br>
-      <input type="password" v-model="password"><br>
-      <div class="Approval">
-        <button v-on:click="login" class="login">ログイン</button>
-        <button v-on:click="register" class="register">新規登録</button>
-        <button v-on:click="googleLogin" class="google"> googleでログイン</button>
+    <div class="userInfo" v-else>
+      <img class="mainlogo" src="~/assets/logo.png" />
+      <br />
+      <div class="mail">
+        <input type="text" placeholder="メールアドレス" v-model="email" />
+      </div>
+      <div class="pass">
+        <input type="password" placeholder="パスワード" v-model="password" />
+      </div>
+      <div class="login">
+        <button v-on:click="login">ログイン</button>
+      </div>
+      <p class="loginWith">You can also login with …</p>
+      <div class="register">
+        <button v-on:click="register">新規登録</button>
+      </div>
+      <div class="googleBotton">
+        <button v-on:click="googleLogin" class="google">google</button>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-import firebase from '~/plugins/firebase'
-import { mapActions, mapState, mapGetters } from 'vuex'
+import firebase from "~/plugins/firebase";
+import { mapActions, mapState, mapGetters } from "vuex";
 export default {
   data() {
     return {
-      email: '',
-      password: ''
-    }
+      email: "",
+      password: ""
+    };
   },
   computed: {
-    ...mapState(['user']),
-    ...mapGetters(['isAuthenticated'])
+    ...mapState(["user"]),
+    ...mapGetters(["isAuthenticated"])
   },
   mounted() {
-    firebase.auth().onAuthStateChanged((user) => {
-      this.setUser(user)
-    })
+    firebase.auth().onAuthStateChanged(user => {
+      this.setUser(user);
+    });
   },
-  methods : {
-    ...mapActions(['setUser']),
-    googleLogin: function(){
-      firebase.auth().signInWithRedirect(new firebase.auth.GoogleAuthProvider());
+  methods: {
+    ...mapActions(["setUser"]),
+    googleLogin: function() {
+      firebase
+        .auth()
+        .signInWithRedirect(new firebase.auth.GoogleAuthProvider());
     },
     login() {
-      firebase.auth().signInWithEmailAndPassword(this.email, this.password)
-      .then(user => {
-        // ログインしたら飛ぶページを指定
-        // this.$router.push("/member-page")
-      }).catch((error) => {
-        alert(error)
-      });
+      firebase
+        .auth()
+        .signInWithEmailAndPassword(this.email, this.password)
+        .then(user => {
+          // ログインしたら飛ぶページを指定
+          // this.$router.push("/member-page")
+        })
+        .catch(error => {
+          alert(error);
+        });
     },
     logout() {
-      firebase.auth().signOut()
-      .then(() => {
-        this.setUser(null)
-      }).catch((error) => {
-        alert(error)
-      })
+      firebase
+        .auth()
+        .signOut()
+        .then(() => {
+          this.setUser(null);
+        })
+        .catch(error => {
+          alert(error);
+        });
     },
     register() {
-      var mailAddress = document.getElementById('mailAddress').value;
-      var password = document.getElementById('password').value;
-  
-      firebase.auth().createUserWithEmailAndPassword(mailAddress, password)
-      .catch(function(error) {
-      alert('登録できません（' + error.message + '）');
-      });
+      var mailAddress = document.getElementById("mailAddress").value;
+      var password = document.getElementById("password").value;
+
+      firebase
+        .auth()
+        .createUserWithEmailAndPassword(mailAddress, password)
+        .catch(function(error) {
+          alert("登録できません（" + error.message + "）");
+        });
     }
   }
-}
+};
 </script>
 <style scoped>
-@media screen and (max-width: 720px){
+@media screen and (max-width: 720px) {
+  .mainlogo {
+    height: 50px;
+    margin: 30px 0px 90px 0px;
+  }
 
-button {
+  button {
     display: block;
-}
+    color: #ffff;
+    background-color: #00cdff;
+    padding: 5px 70px;
+    border-radius: 7px;
+    outline: 0;
+    border: none;
+    margin: auto;
+  }
 
+  .google {
+    background-color: #dd4b39;
+  }
+
+  input {
+    text-align: left;
+    padding: 3px 25px;
+    border-radius: 7px;
+    border: solid 0.7px #b8b8b8;
+  }
+
+  .userInfo {
+    text-align: center;
+  }
+
+  .loginWith {
+    margin-bottom: 40px;
+  }
+
+  .login,.register,.googleBotton {
+    margin-bottom: 25px;
+  }
+
+  .mail {
+    margin-bottom: 20px;
+  }
+
+  .pass {
+    margin-bottom: 50px;
+  }
+
+  .loginTitle {
+    margin-top: 30px;
+    margin-bottom: 50px;
+    text-align: center;
+    font-weight: 700;
+    color: #00cdff;
+    font-size: 22px;
+  }
 }
 </style>
